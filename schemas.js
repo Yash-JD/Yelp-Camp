e26260425238a315c69907a8ca70ba79d626bce5
Yash <yjdobariya0@gmail.com>
2024-04-21 18:50:45 +0530

    add error handler

---
+++ schemas.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+// define a schema using joi to validate mongodb(server-side) error    
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+    }).required()
+});
+
