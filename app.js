f44394607dc87fdc39d6ca5da90a96130cf7f410
Yash <yjdobariya0@gmail.com>
2024-04-30 23:09:59 +0530

    handle middleware error

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');   // client-side error validation
+const ExpressError = require('./utils/ExpressError');   // client-side error validation
 const methodOverride = require('method-override');
 const campground = require('./models/campground');
 
@@ -22,6 +25,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })) // tell the express to parse url encoded to json body
 app.use(methodOverride('_method'));
 
+// define a Joi validation middleware funciton
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)    // this will redirect to server side route handler
+    } else {
+        next();
+    }
+}
+
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -37,36 +51,47 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new.ejs');
 })
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const newCampground = new campground(req.body.campground);
     await newCampground.save();
     res.redirect(`/campgrounds/${newCampground._id}`)
-})
+}))
 
 // view particular camp
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const camp = await campground.findById(req.params.id);
     res.render('campgrounds/show.ejs', { camp });
-})
+}))
 
 // to edit/update a camp
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const camp = await campground.findById(req.params.id);
     res.render('campgrounds/edit.ejs', { camp });
-})
-app.put('/campgrounds/:id', async (req, res) => {
+}))
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     // below req.body.campground is the object(in ejs files see name is campgrond[...])
     // and "..." is used to spread the object 
     const camp = await campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
     res.redirect(`/campgrounds/${camp._id}`)
-})
+}))
 
 // to delele a camp
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await campground.findByIdAndDelete(req.params.id);
     res.redirect(`/campgrounds`)
+}))
+
+// error handler for any user route error
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
+// error handling for server side
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;   // default value
+    if (!err.message) err.message = 'Something went wrong!';
+    res.status(statusCode).render('error', { err });
+})
 
 app.listen(3000, () => {
     console.log("Listening on PORT 3000")
