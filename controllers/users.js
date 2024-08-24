67365bcc86a1ee722ba631a7901140be129da9db
Yash <yjdobariya0@gmail.com>
2024-08-24 22:32:39 +0530

    add basic security stuff

--- controllers/users.js
+++ controllers/users.js
@@ -33,9 +33,10 @@ module.exports.login = (req, res) => {
 }
 
 module.exports.logout = (req, res) => {
-    req.logout(function (err) {
-        if (err) { return next(err); }
+    // req.logout(function (err) {
+    //     if (err) { return next(err); }
+    req.logout();
+    // req.session.destroy();
         req.flash('success', "Logged out!");
         res.redirect('/campgrounds');
-    });
 }
