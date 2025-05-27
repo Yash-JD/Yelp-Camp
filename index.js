bf9a31f80a0ac4befeee25e929bf2c14caa5a1de
Yash <yjdobariya0@gmail.com>
2025-05-27 17:12:00 +0530

    db url changed

--- index.js
+++ index.js
@@ -120,9 +120,9 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-// app.listen(3000, () => {
-//   console.log("Listening on PORT 3000");
-// });
+app.listen(3000, () => {
+  console.log("Listening on PORT 3000");
+});
 
 // For Vercel, export the app as a serverless function
-module.exports = app;
+// module.exports = app;
